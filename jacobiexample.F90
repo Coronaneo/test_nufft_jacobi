4a64d12e15c870ab1c1a6f6414f2bcf899e9d9bc
Pangqiyuangh <ppangqqyz@foxmail.com>
2018-05-23 02:08:50 -0700

    new start

--- jacobiexample.F90
+++ jacobiexample.F90
@@ -14,33 +14,40 @@ mwPointer    :: plhs(*), prhs(*)
 ! get some of the matlab mex functions
 mwPointer    :: mxGetPr, mxGetPi, mxCreateDoubleMatrix 
 ! define a size integer so that we can get its type
-mwSize       :: nj
+mwSize       :: nj,nts
 
 type(chebexps_data)           :: chebdata
 real*8, allocatable :: ab(:,:)
 real*8, allocatable :: psivals(:),avals(:)
 real*8, allocatable :: ts(:),avals0(:),psivals0(:),polvals(:),polvals0(:)
-real*8, allocatable :: xx(:),xs(:),twhts(:)
+real*8, allocatable :: xs(:),twhts(:),tss(:,:)
 complex*16,allocatable :: jacobi1(:,:),jacobi2(:,:)
-integer*4 nts,k
+real*8 :: nts1,da,db
+integer*4  k,nt
 real*8 pi
 
+!allocate(nts1(2,1))
 pi  = acos(-1.0d0)
-call mxCopyPtrToInteger4(mxGetPr(prhs(1)),nts,1)
+!call mxCopyPtrToReal8(mxGetPr(prhs(1)),nts1,1)
+!nts = ceiling(nts1(1,1))
+nts = mxGetM(prhs(1))
+nt = nts
 !nts = 2**9
 allocate(jacobi1(nts,nts-27),jacobi2(nts,nts-27))
-allocate(ts(nts),xs(nts),twhts(nts),avals0(nts))
+allocate(ts(nts),xs(nts),twhts(nts),avals0(nts),tss(nts,1))
 allocate(psivals0(nts),polvals(nts),polvals0(nts))
 
-da=0.25d0
-db=0.25d0
+!da=0.25d0
+!db=0.25d0
+call mxCopyPtrToReal8(mxGetPr(prhs(2)),da,1)
+call mxCopyPtrToReal8(mxGetPr(prhs(3)),db,1)
 call jacobi_quad_mod(nts,da,db,ts,twhts)
 
 do i=1,nts
    xs(i) = (mod(floor(ts(i)/2/pi*nts+0.5),nts))*2*pi/nts
 end do
-call quicksort(nts,ts)
-call quicksort(nts,xs)
+!call quicksort(nt,ts)
+!call quicksort(nt,xs)
 
 
 
@@ -84,14 +91,14 @@ call jacobi_phase_eval(chebdata,dnu,da,db,nints,ab,avals,psivals,nts,ts,avals0,p
 !call prin2("average eval time = ",(t2-t1)/nts)
 !polvals0 = cos(psivals0)*avals0
 !print *,size(avals0),size(psivals0)
-jacobi1(:,i-26)=avals0*exp(dcmplx(0,1)*(psivals0-i*ts))
-jacobi2(:,i-26)=avals0*exp(dcmplx(0,1)*(psivals0-i*xs))
+jacobi1(:,i-26)=avals0*exp(dcmplx(0,1)*(psivals0-i*ts))*twhts
+jacobi2(:,i-26)=avals0*exp(dcmplx(0,1)*(psivals0-i*xs))*twhts
 end do
 
 plhs(1) = mxCreateDoubleMatrix(nts, 1, 0)
 plhs(2) = mxCreateDoubleMatrix(nts, nts-27, 1)
 plhs(3) = mxCreateDoubleMatrix(nts, nts-27, 1)
-
+!tss(:,1) = ts
 call mxCopyReal8ToPtr(ts, mxGetPr(plhs(1)),nts) 
 call mxCopyComplex16ToPtr(jacobi1, mxGetPr(plhs(2)),mxGetPi(plhs(2)),nts*(nts-27)) 
 call mxCopyComplex16ToPtr(jacobi2, mxGetPr(plhs(3)),mxGetPi(plhs(3)),nts*(nts-27)) 
--- testjacex.m
+++ testjacex.m
@@ -1,34 +1,74 @@
-nts=2^9
-tR=60;
-mR=60;
-tol=1e-12
-[ts jacobi1 jacobi2]=jacobiexample(nts);
-jacobi1=[zeros(nts,27) jacobi1];
-jacobi2=[zeros(nts,27) jacobi2];
-[U1,V1]=lowrank(jacobi1,tol,tR,mR);
-[U2,V2]=lowrank(jacobi2,tol,tR,mR);
-rank1=size(U1,2)
-rank2=size(U2,2)
-c=rand(nts,1);
-ncol = size(c,2);
+format long
+num=10;
+da=0.25;
+db=0.30;
+tol=1e-4
+str1='size';
+str2='our_rank';
+str3='nyu_rank';
+str4='our_time';
+str5='nyu_time';
+str6='ratio';
+str7='error_our';
+str8='error_nyu';
+str9='dir_time';
+fprintf('\n');
+fprintf('%-6s%-11s%-11s%-15s%-15s%-14s%-15s%-14s%-10s\n',str1,str2,str3,str4,str5,str6,str7,str8,str9);
+
+for m=9:20
+    nts=2^m;
+    nt=zeros(nts,1);
+    nn=[nts,0]';
+    [ts,jacobi1,jacobi2] = jacobiexample(nt,da,db);
+    %s=round(nts*ts);
+    %gamma=norm(nts*ts-s,inf);
+    %xi=log(log(10/tol)/gamma/7);
+    %lw=xi-log(xi)+log(xi)/xi+0.5*log(xi)^2/xi^2-log(xi)/xi^2;
+    %K=ceil(5*gamma*exp(lw));
+    tR=3*nts/4;
+    mR=3*nts/4;
+    jacobi1=[zeros(nts,27) jacobi1];
+    jacobi2=[zeros(nts,27) jacobi2];
+    [U1,V1]=lowrank(jacobi1,tol,tR,mR);
+    [U2,V2]=lowrank(jacobi2,tol,tR,mR);
+    rank1=size(U1,2);
+    rank2=size(U2,2);
+    c=rand(nts,1);
+    ncol = size(c,2);
 
 
-xs=mod(round(ts*nts/2/pi),nts)+1;
-d = repmat(conj(V2),1,ncol).*reshape(repmat(c,rank2,1),nts,rank2*ncol);
-fftc = ifft(d);
-fftc = fftc(xs,:);
-result2 = nts*squeeze(sum(reshape(repmat(U2,1,ncol).*fftc,nts,rank2,ncol),2));
+    xs=mod(round(ts*nts/2/pi),nts)+1;
+    tic;
+    for j=1:num
+        d = repmat(conj(V2),1,ncol).*reshape(repmat(c,rank2,1),nts,rank2*ncol);
+        fftc = ifft(d);
+        fftc = fftc(xs,:);
+        result2 = nts*squeeze(sum(reshape(repmat(U2,1,ncol).*fftc,nts,rank2,ncol),2));
+    end
+    timeour=toc/num;
 
-ex = exp(1i*nts/2*ts);
-result1=zeros(nts,1)
-for i=1:rank1
-   cj = nufft1dIInyumex(ts,1,tol,conj(V1(:,i))*c);
-   result1 = result1 + U1(:,i).*ex.*cj;
+    ex = exp(1i*nts/2*ts);
+    U1=U1.*repmat(ex,1,rank1);
+    tic;
+    for j=1:num
+        result1=zeros(nts,1);
+        for i=1:rank1
+            cj = nufft1dIInyumex(ts,1,tol,conj(V1(:,i)).*c);
+            result1 = result1 + U1(:,i).*cj;
+        end
+    end
+    timenyu=toc/num;
+    timeratio=timeour/timenyu;
+
+    k=[0:nts-1];
+    F2=exp(1i*ts*k);
+    tic;
+    for i=1:num
+    result3=(jacobi1.*F2)*c;
+    end
+    timedir=toc/num;
+
+    errornyu=norm(result1-result3)/norm(result3);
+    errorour=norm(result2-result3)/norm(result3);
+    fprintf('\n   %-5d %-9d  %-9d  %-1.6E   %-1.6E   %-1.6E  %-1.6E   %-1.6E  %-1.6E\n',m,rank2,rank1,timeour,timenyu,timeratio,errorour,errornyu,timedir);
 end
-
-k=[0:nts-1];
-F2=exp(1i*(ts.')*k);
-result3=(jacobi1.*F2)*c;
-
-error13=norm(result1-result3)/norm(result3)
-error23=norm(result2-result3)/norm(result3)
