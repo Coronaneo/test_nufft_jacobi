a9573ab1392062727608924db70bdbe6d36dec86
Pangqiyuangh <ppangqqyz@foxmail.com>
2018-11-09 21:37:15 +0800

    modify invN pre

--- testinvNJPT1D.m
+++ testinvNJPT1D.m
@@ -12,7 +12,7 @@ fprintf('\n');
 fprintf('start 1D inverse nonuniform Jacobi polynomial transform test:');
 fprintf('\n');
 fprintf('da = %1.2f,db = %1.2f\n',da,db);
-fprintf('%-6s%-11s%-15s%-15s%-15s\n',str1,str2,str3,str4,str5);
+fprintf('%-6s%-11s%-10s%-15s%-15s\n',str1,str2,str3,str4,str5);
 for m=7:13
     nts=2^m;
     ts = unique(rand(nts,1)*(pi-2/nts)+1/nts);
@@ -23,5 +23,5 @@ for m=7:13
         [y,flag,relres,iter] = yfun(b);
     end
     time = toc/num;
-    fprintf('\n %-5d %-9d %-1.6E %-1.6E %-1.6E\n',m,flag,iter,relres,time);
+    fprintf('\n %-5d %-9d %-3d %-3d   %-1.6E        %-1.6E\n',m,flag,iter,relres,time);
 end
