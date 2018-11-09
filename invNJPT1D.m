ed12c8c9927678ff2b188e9db4c73848e41381ff
Pangqiyuangh <ppangqqyz@foxmail.com>
2018-11-09 21:47:17 +0800

    modify invN pre

--- invNJPT1D.m
+++ invNJPT1D.m
@@ -1,7 +1,7 @@
 function [yfun,fun_pre] = invNJPT1D(nts,ts,da,db,tol)
 
 nt = zeros(nts,1);
-nu = [0:nts-1];
+nu = [0:nts-1]';
 wghts = ones(nts,1);
 n1 = [1:nts];
 fun = @(x)directjac1(nt,x,da,db,n1,ts,nu,wghts);
@@ -11,7 +11,9 @@ A = extrjac1(nt,rs,cs,-1,da,db,ts,nu,wghts);
 [U,S,V] = svd(A,'econ');
 u = sqrt(S(1,1))*U(:,1);
 v = sqrt(S(1,1))*conj(V(:,1));
-fun_pre = @(y)invNJPT1Dpre(nts,ts,nu,da,db,u,v,y,tol);
+N = exp(1i*ts*nu');
+M = N'*N;
+fun_pre = @(y)invNJPT1Dpre(nts,ts,nu,da,db,u,v,y,tol,N,M);
 restart = 5;
 maxit =  5;
 yfun = @(b)gmres(fun,b,restart,tol,maxit,fun_pre);
--- invNJPT1Dpre.m
+++ invNJPT1Dpre.m
@@ -1,4 +1,4 @@
-function [x] = invNJPT1Dpre(nts,ts,nu,da,db,u,v,y,tol)
+function [x] = invNJPT1Dpre(nts,ts,nu,da,db,u,v,y,tol,N,M)
 %nt = zeros(nts,1);
 %rs = [1:nts]';
 %cs = [1:nts]';
@@ -9,9 +9,7 @@ function [x] = invNJPT1Dpre(nts,ts,nu,da,db,u,v,y,tol)
 %u = sqrt(S(1,1))*U(:,1);
 %v = sqrt(S(1,1))*conj(V(:,1));
 y1 = y./u;
-N = exp(1i*ts*nu');
 y2 = (N')*y1;
-M = N'*N;
 [x1,flag,relres,iter] = cgs(M,y2,tol);
 x = x1./v;
 end
