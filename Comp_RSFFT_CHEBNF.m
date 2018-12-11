28d70e2da2aa82a2a019bd81b8edda228665c54a
Pangqiyuangh <ppangqqyz@foxmail.com>
2018-12-10 21:38:44 -0800

    rank

--- Comp_RSFFT_CHEBNF.m
+++ Comp_RSFFT_CHEBNF.m
@@ -244,13 +244,14 @@ end
     figure('visible','off');
     pic = figure;
     hold on;
-    ag = (log2(timeour1(1))+log2(timeour2(1))+log2(timeour3(1)))/3;
+    %ag = (log2(timeour1(1))+log2(timeour2(1))+log2(timeour3(1)))/3;
+    ag = (log2(timeour1(1))+log2(timeour3(1)))/2;
     h(1) = plot(vd,vd+log2(vd)-vd(1)-log2(vd(1))+ag,'--c','LineWidth',4);
     h(2) = plot(vd,vd+2*log2(vd)-vd(1)-2*log2(vd(1))+ag,'--k','LineWidth',4);
     h(3) = plot(vd,log2(timeour1),'-^r','LineWidth',2);
-    h(4) = plot(vd,log2(timeour2),'-^b','LineWidth',2);
-    h(5) = plot(vd,log2(timeour3),'-^g','LineWidth',2);
-    legend('N log N','N log^2 N','NP2 app','NP1 app','NP0 app','Location','bestoutside');
+    %h(4) = plot(vd,log2(timeour2),'-^b','LineWidth',2);
+    h(4) = plot(vd,log2(timeour3),'-^g','LineWidth',2);
+    legend('N log N','N log^2 N','NP1 app','NP0 app','Location','bestoutside');
     %if flag > 0
     %   title('RS FFT vs CHEB NUFFT time, uni JPT');
     %else
@@ -262,20 +263,21 @@ end
     b=get(gca);
     set(b.XLabel, 'FontSize', 20);set(b.YLabel, 'FontSize', 20);set(b.ZLabel, 'FontSize', 20);set(b.Title, 'FontSize', 20);
     if flag > 0
-       saveas(pic,['Comp_RSFFT_CHEBNF_uni1_-095.eps'],'epsc');
+       saveas(pic,['Comp_RSFFT_CHEBNF_uni1.eps'],'epsc');
     else 
        saveas(pic,['Comp_RSFFT_CHEBNF_non1.eps'],'epsc');
     end
     hold off;
     pic = figure;
     hold on;
-    ag = (log2(timefac1(1))+log2(timefac1(1))+log2(timefac3(1)))/3;
+    %ag = (log2(timefac1(1))+log2(timefac1(1))+log2(timefac3(1)))/3;
+    ag = (log2(timefac1(1))+log2(timefac3(1)))/2;
     h(1) = plot(vd,vd+log2(vd)-vd(1)-log2(vd(1))+ag,'--c','LineWidth',4);
     h(2) = plot(vd,vd+2*log2(vd)-vd(1)-2*log2(vd(1))+ag,'--k','LineWidth',4);
     h(3) = plot(vd,log2(timefac1),'-xr','LineWidth',2);
-    h(4) = plot(vd,log2(timefac2),'-xb','LineWidth',2);
-    h(5) = plot(vd,log2(timefac3),'-xg','LineWidth',2);
-    legend('N log N','N log^2 N','NP2 fac','NP1 fac','NP0 fac','Location','bestoutside');
+    %h(4) = plot(vd,log2(timefac2),'-xb','LineWidth',2);
+    h(4) = plot(vd,log2(timefac3),'-xg','LineWidth',2);
+    legend('N log N','N log^2 N','NP1 fac','NP0 fac','Location','bestoutside');
     %if flag > 0
     %   title('RS FFT vs CHEB NUFFT time, uni JPT');
     %else
@@ -287,7 +289,7 @@ end
     b=get(gca);
     set(b.XLabel, 'FontSize', 20);set(b.YLabel, 'FontSize', 20);set(b.ZLabel, 'FontSize', 20);set(b.Title, 'FontSize', 20);
     if flag > 0
-       saveas(pic,['Comp_RSFFT_CHEBNF_uni2_-095.eps'],'epsc');
+       saveas(pic,['Comp_RSFFT_CHEBNF_uni2.eps'],'epsc');
     else 
        saveas(pic,['Comp_RSFFT_CHEBNF_non2.eps'],'epsc');
     end
@@ -295,9 +297,9 @@ end
     pic1 = figure;
     hold on;
     h(1) = plot(vd,log10(errorour1),'-^r','LineWidth',2);
-    h(2) = plot(vd,log10(errorour2),'-^b','LineWidth',2);
-    h(3) = plot(vd,log10(errorour3),'-^g','LineWidth',2);
-    legend('NP2 relerr','NP1 relerr','NP0 relerr','Location','bestoutside');
+    %h(2) = plot(vd,log10(errorour2),'-^b','LineWidth',2);
+    h(2) = plot(vd,log10(errorour3),'-^g','LineWidth',2);
+    legend('NP1 relerr','NP0 relerr','Location','bestoutside');
     %if flag > 0
     %   title('relerr, uni JPT');
     %else
@@ -309,7 +311,7 @@ end
     b=get(gca);
     set(b.XLabel, 'FontSize', 20);set(b.YLabel, 'FontSize', 20);set(b.ZLabel, 'FontSize', 20);set(b.Title, 'FontSize', 20);
     if flag > 0
-       saveas(pic1,['CompFN_relerr_uni_-095.eps'],'epsc');
+       saveas(pic1,['CompFN_relerr_uni.eps'],'epsc');
     else 
        saveas(pic1,['CompFN_relerr_non.eps'],'epsc');
     end
@@ -317,9 +319,9 @@ end
     pic2 = figure;
     hold on;
     h(1) = plot(vd,rank1,'-^r','LineWidth',2);
-    h(2) = plot(vd,rank2,'-^b','LineWidth',2);
-    h(3) = plot(vd,rank3,'-^g','LineWidth',2);
-    legend('NP2 rank','NP1 rank','NP0 rank','Location','bestoutside');
+    %h(2) = plot(vd,rank2,'-^b','LineWidth',2);
+    h(2) = plot(vd,rank3,'-^g','LineWidth',2);
+    legend('NP1 rank','NP0 rank','Location','bestoutside');
     %if flag > 0
     %   title('rank, uni JPT');
     %else
@@ -331,7 +333,7 @@ end
     b=get(gca);
     set(b.XLabel, 'FontSize', 20);set(b.YLabel, 'FontSize', 20);set(b.ZLabel, 'FontSize', 20);set(b.Title, 'FontSize', 20);
     if flag > 0
-       saveas(pic2,['CompFN_rank_uni_-095.eps'],'epsc');
+       saveas(pic2,['CompFN_rank_uni.eps'],'epsc');
     else 
        saveas(pic2,['CompFN_rank_non.eps'],'epsc');
     end
